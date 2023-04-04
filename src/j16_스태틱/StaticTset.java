f6a3b51f7f479839cee731fb58a7bfbe43af8f59
DESKTOP-F3GI7IK\User <cjfals8570@naver.com>
2023-04-04 19:06:04 +0900

    자바 기초 복습 10-1

---
+++ src/j16_스태틱/StaticTset.java
@@ -0,0 +1,14 @@
+package j16_스태틱;
+
+public class StaticTset {
+	public static void test() {
+		System.out.println("test");
+	}
+
+	public static void main(String[] args) {
+		StaticEx.name = "이철민";
+		StaticEx2.print();
+
+	}
+
+}
